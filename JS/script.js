19d0b8f1a4cf4a7664be78ac5f68856df4b8317e
Ibrahim Wahab <Ibrahim.m.ibrahim2009@gmail.com>
2026-09-18 20:57:49 +0300

    header done, it will need colors and font type like the other future strucures, but will be set later.

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+let burger = document.getElementById("burger");
+let dropUl = document.getElementById("dropUl");
+burger.addEventListener("click", function () {
+  dropUl.classList.toggle("active");
+});
