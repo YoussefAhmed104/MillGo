ba286cee0f6c28a45e97e08f703f52b6c5a78252
Ibrahim Wahab <Ibrahim.m.ibrahim2009@gmail.com>
2026-09-18 23:27:41 +0300

    still working on the form

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,17 @@ let dropUl = document.getElementById("dropUl");
 burger.addEventListener("click", function () {
   dropUl.classList.toggle("active");
 });
+
+let selectedRadio = null;
+let radios = document.querySelectorAll('input[type="radio"]');
+
+radios.forEach(function (radio) {
+  radio.addEventListener("click", function () {
+    if (selectedRadio === radio) {
+      radio.checked = false;
+      selectedRadio = null;
+    } else {
+      selectedRadio = radio;
+    }
+  });
+});
